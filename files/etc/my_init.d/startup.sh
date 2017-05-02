20ec0687581858a6fb84d695dc7c5b073745cb7f
Tom Noogen <tom@tnguyen-imac.local>
2017-05-02 14:27:07 -0500

    update conf

--- files/etc/my_init.d/startup.sh
+++ files/etc/my_init.d/startup.sh
@@ -6,6 +6,9 @@ export TERM=xterm
 env > /root/env.txt
 
 mkdir -p /etc/nginx/sites-enabled
+mkdir -p /tmp/nginx/cache
+chown -R www-data:nginx /tmp/nginx
+
 service nginx start
 
 bash /root/bin/my-startup.sh
