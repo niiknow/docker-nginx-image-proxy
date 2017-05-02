5c0864e1f4725884be46c018e1476875c26736be
Tom Noogen <tom@tnguyen-imac.local>
2017-05-02 13:17:19 -0500

    update url parsing

--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -14,6 +14,13 @@ log() {
   else echo; fi
 }
 
+safe_reload() {
+  echo "[`date +'%Y-%m-%d %T'`] nginx safe reload";
+
+  nginx -t &&
+    service nginx reload # only runs if nginx -t succeeds
+}
+
 rm -f /etc/nginx/sites-enabled/server.conf
 
 if [ -z "$SERVER_CONF" ] ; then
@@ -22,4 +29,4 @@ else
    curl -SL $SERVER_CONF --output /etc/nginx/sites-enabled/server.conf
 fi
 
-nginx -s reload
+safe_reload();
