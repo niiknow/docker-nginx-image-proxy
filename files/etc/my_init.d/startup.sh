ec7a605fa2f3c4a2a3955bb03d2b7647f5ca6f29
Tom Noogen <tom@tnguyen-imac.local>
2017-05-02 10:32:47 -0500

    update server conf

--- files/etc/my_init.d/startup.sh
+++ files/etc/my_init.d/startup.sh
@@ -5,6 +5,7 @@ export TERM=xterm
 # save environment variables for use later
 env > /root/env.txt
 
+mkdir -p /etc/nginx/sites-enabled
 service nginx start
 
 bash /root/bin/my-startup.sh
--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -14,12 +14,12 @@ log() {
   else echo; fi
 }
 
-if [ -z "$WHITELIST_HOSTS" ] ; then
-# wildcard default
-  WHITELIST_HOSTS=".*"
+rm -f /etc/nginx/sites-enabled/server.conf
+
+if [ -z "$SERVER_CONF" ] ; then
+   cp /root/server.conf /etc/nginx/sites-enabled/server.conf
+else
+   curl -SL $SERVER_CONF --output /etc/nginx/sites-enabled/server.conf
 fi
 
-rm -f /etc/nginx/sites-enabled/default.conf
-cp /etc/nginx/sites-available/default.tpl /etc/nginx/sites-enabled/default.conf
-sed -i "s/___MY_WHITELIST_HOSTS___/$WHITELIST_HOSTS/g" /etc/nginx/sites-enabled/default.conf
 nginx -s reload
