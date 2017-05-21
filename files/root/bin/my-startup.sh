ebaf602b0771d51878d82c82cb7b0375ddbe28b4
Tom Noogen <tomn@Toms-MacBook-Pro.local>
2017-05-21 18:10:45 -0500

    better matching

--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -13,14 +13,15 @@ log() {
    if [[ "$@" ]]; then echo "[`date +'%Y-%m-%d %T'`] $@";
    else echo; fi
 }
+regex='^(https?|ftp|file):\/\/'
 
-if [ -n "$SERVER_CONF" ] ; then
+if [[ "$SERVER_CONF" =~ regex ]] ; then
    echo "[`date +'%Y-%m-%d %T'`] Getting new server conf"
    mv /etc/nginx/sites-enabled/server.conf /etc/nginx/sites-enabled/server.bak
    curl -SL $SERVER_CONF --output /etc/nginx/sites-enabled/server.conf
 fi
 
-if [ -n "$GEODB_URL" ] ; then
+if [[ "$GEODB_URL" =~ regex ]]  ; then
    echo "[`date +'%Y-%m-%d %T'`] Updating geo db"
    curl $GEODB_URL | gzip -d - > /etc/nginx/GeoLiteCity.dat
 fi
