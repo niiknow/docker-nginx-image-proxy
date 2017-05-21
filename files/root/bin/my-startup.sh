781fa995408a8fce4f1f0d6d6a0e78f9d6f7b20a
Tom Noogen <tomn@Toms-MacBook-Pro.local>
2017-05-21 18:22:44 -0500

    revert matching

--- files/root/bin/my-startup.sh
+++ files/root/bin/my-startup.sh
@@ -13,16 +13,15 @@ log() {
    if [[ "$@" ]]; then echo "[`date +'%Y-%m-%d %T'`] $@";
    else echo; fi
 }
-regex='^(https?):\/\/'
 
-if [[ "$SERVER_CONF" =~ regex ]] ; then
-   echo "[`date +'%Y-%m-%d %T'`] Getting new server conf"
+if [ -n "$SERVER_CONF" ] ; then
+   log "Getting new server conf"
    mv /etc/nginx/sites-enabled/server.conf /etc/nginx/sites-enabled/server.bak
    curl -SL $SERVER_CONF --output /etc/nginx/sites-enabled/server.conf
 fi
 
-if [[ "$GEODB_URL" =~ regex ]]  ; then
-   echo "[`date +'%Y-%m-%d %T'`] Updating geo db"
+if [ -n "$GEODB_URL" ] ; then
+   log "Updating geo db"
    curl $GEODB_URL | gzip -d - > /etc/nginx/GeoLiteCity.dat
 fi
 
