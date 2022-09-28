e4b841579346c5d55b538f7a2ffa2e40a173266f
noogen <tom@noogen.net>
2022-09-28 10:07:05 -0500

    update geoip2 update

--- files/etc/nginx/geoip2-download.sh
+++ files/etc/nginx/geoip2-download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+cd /etc/nginx
 curl -sLo GeoLite2-ASN.mmdb https://git.io/GeoLite2-ASN.mmdb
 curl -sLo GeoLite2-City.mmdb https://git.io/GeoLite2-City.mmdb
 curl -sLo GeoLite2-Country.mmdb https://git.io/GeoLite2-Country.mmdb
